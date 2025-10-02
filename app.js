ca7950e4e42270e4242a74c236f886fda0e55e0e
ShriRadhey-Mishra <radheymishra13@gmail.com>
2025-10-02 15:58:36 +0530

    env

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-app.use(morgan('dev'));
+
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+
 app.use(express.json());
 app.use((req, res, next) => {
   console.log('Hello from the middleware.');
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const app = require('./app');
+const dotenv = require('dotenv');
 
-const PORT = 3000;
+dotenv.config({
+    path: './config.env'
+});
+
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+// console.log(process.env);
 
 app.listen(PORT, () => {
   console.log(`App running at "http://127.0.0.1:${PORT}/" ...`);
